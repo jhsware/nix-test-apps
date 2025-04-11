2d373641f0bda01f0aead9769b108aeeb9373d78
Sebastian Ware <sebastian@urbantalk.se>
2025-04-11 18:12:29 +0200

    Fix mariadb app

--- hello-mariadb/src/db.js
+++ hello-mariadb/src/db.js
@@ -2,22 +2,40 @@ const mysql = require('mysql2/promise');
 
 const IS_PROD = process.env.NODE_ENV === "production";
 const {
-  CONNECTION_STRING = "mysql://root:password@localhost:3306/test"
+  CONNECTION_STRING // = "mysql://root:password@localhost:3306/test"
 } = process.env;
 
 if (IS_PROD && !CONNECTION_STRING)
   throw new Error("Missing env var CONNECTION_STRING");
 
-const dbConnectionUrl = CONNECTION_STRING;
+const regex = /^(mysql|mariadb):\/\/.*@(.*)\/.*$/;
+const match = CONNECTION_STRING.match(regex);
+const hostParts = match[2].split(',');
+const tmpUri = CONNECTION_STRING.replace(match[2], hostParts[0]);
+const baseUrlParts = new URL(tmpUri);
 
-console.log(`Connecting to ${dbConnectionUrl}`);
+console.log(`Connecting to ${match[2]}`);
 
 class Database {
   _pool = undefined;
 
   async connect() {
-    if (this._pool) return;
-    this._pool = mysql.createPool(dbConnectionUrl);
+    if (!this._pool) {
+      this._pool = mysql.createPoolCluster();
+      const hostNames = ['Primary', 'Secondary_1', 'Secondary_2'];
+      for (const index in hostParts) {
+        const hostName = hostNames[index];
+        const host = hostParts[index];
+        this._pool.add(hostName, {
+          host: host,
+          user: baseUrlParts.username,
+          database: baseUrlParts.pathname,
+          password: baseUrlParts.password,
+        });
+      }
+    }
+
+    return await this._pool.getConnection();
   }
 
   async close() {
@@ -28,8 +46,8 @@ class Database {
   }
 
   async fetchById(id, tableName) {
-    await this.connect();
-    const [rows] = await this._pool.execute(
+    const conn = await this.connect();
+    const [rows] = await conn.execute(
       `SELECT * FROM ${tableName} WHERE id = ?`, 
       [id]
     );
@@ -37,12 +55,12 @@ class Database {
   }
 
   async insert(doc, tableName) {
-    await this.connect();
+    const conn = await this.connect();
     const columns = Object.keys(doc).join(', ');
     const placeholders = Object.keys(doc).map(() => '?').join(', ');
     const values = Object.values(doc);
     
-    const [result] = await this._pool.execute(
+    const [result] = await conn.execute(
       `INSERT INTO ${tableName} (${columns}) VALUES (${placeholders})`,
       values
     );
